defb9f49d1308831f44758e86fb92ddb6ba3f17e
Горьков Владислав <vladeys717@gmail.com>
2024-12-24 15:14:59 +0500

    Create currency file from CB API

---
+++ my_project/my_project_app/py_scripts/make_currency_csv.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import requests
+import csv
+from lxml import etree
+
+
+currency_URL = 'https://www.cbr.ru/scripts/XML_daily.asp'
+required_currency = ['BYR', 'USD', 'EUR', 'KZT', 'UAH', 'AZN', 'KGS', 'UZS', 'GEL']
+
+
+def get_info(tree, one_currency):
+    required_information = tree.xpath(f"//Valute[CharCode='{one_currency}']")
+    if not required_information:
+        return None
+
+    currency_value = float(required_information[0].find('Value').text.replace(',', '.'))
+    currency_nominal = int(required_information[0].find('Nominal').text)
+    prepared_value = round(currency_value / currency_nominal, 8)
+    return prepared_value
+
+
+def prepare_currency_information(date):
+    response = requests.get(f'{currency_URL}?date_req={date.strftime("%d/%m/%Y")}')
+    current_tree = etree.fromstring(response.content)
+    current_row = [date.strftime('%Y-%m')]
+
+    for one_currency in required_currency:
+        current_row.append(get_info(current_tree, one_currency))
+    return current_row
+
+
+def main():
+    start_date = pd.to_datetime('2003.01.01')
+    end_date = pd.to_datetime('2024.12.01')
+    date_offset = pd.DateOffset(months=1)
+    current_date = start_date
+
+    with open('csv_prepared_files/currency_values.csv', mode='w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['date'] + required_currency)
+        while current_date <= end_date:
+            row = prepare_currency_information(current_date)
+            writer.writerow(row)
+            current_date += date_offset
+
+
+if __name__ == "__main__":
+    main()
