dd52e4875e8793479a23a6e6ee79a91b6e22ab56
Горьков Владислав <vladeys717@gmail.com>
2025-01-13 12:04:43 +0500

    last vacancies from hh ru

--- my_project/my_project_app/views.py
+++ my_project/my_project_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import requests
+from datetime import datetime, timedelta
 from my_project_app.models import *
 
 
@@ -90,4 +92,78 @@ def top_skills_page(request):
 
 
 def last_vacancies_page(request):
-    return render(request, 'last_vacancies.html')
+    professions = ['analytic', 'аналитик', 'analyst', 'аналітик']
+    url = 'https://api.hh.ru/vacancies'
+    vacancies = []
+    total_vacancies_loaded = 0  # Счетчик загруженных вакансий
+
+    for profession in professions:
+        params = {
+            'text': profession,
+            'per_page': 10,  # Запрашиваем 10 вакансий для каждой профессии
+            'order_by': 'publication_time',
+            'date_from': (datetime.now() - timedelta(days=1)).isoformat(),
+        }
+
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            for item in data['items']:
+                if total_vacancies_loaded >= 10:  # Проверка, если уже загружено 10 вакансий
+                    break  # Выходим из цикла, если достигли лимита
+
+                vacancy_details = fetch_vacancy_details(item['id'])
+                if vacancy_details:
+                    skills = vacancy_details.get('key_skills', [])
+                    skills_string = ', '.join([skill['name'] for skill in skills]) if skills else ''
+                    published_at_str = item.get('published_at')
+                    if published_at_str:
+                        published_at = datetime.fromisoformat(
+                            published_at_str.replace('Z', '+00:00'))  # Исправленная строка
+                    else:
+                        published_at = None
+
+                    vacancies.append({
+                        'name': item['name'],
+                        'description': vacancy_details.get('description', 'Описание отсутствует'),
+                        'skills': skills_string,
+                        'company': item['employer']['name'] if item.get('employer') else "Не указано",
+                        'salary': get_salary_string(item.get('salary', {})),
+                        'area': item['area']['name'],
+                        'published_at': published_at,  # Исправленная строка
+                    })
+                    total_vacancies_loaded += 1  # Увеличиваем счетчик
+
+    return render(request, 'last_vacancies.html', {'vacancies': vacancies})
+
+
+def fetch_vacancy_details(vacancy_id):
+    url = f'https://api.hh.ru/vacancies/{vacancy_id}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
+def get_salary_string(salary_data):
+    if not salary_data:
+        return 'Не указан'
+
+    salary_from = salary_data.get('from')
+    salary_to = salary_data.get('to')
+    currency = salary_data.get('currency')
+
+    salary_str = ''
+
+    if salary_from:
+        salary_str += f'от {salary_from} '
+    if salary_to:
+        salary_str += f'до {salary_to} '
+    if currency:
+        salary_str += currency
+
+    if not salary_str:
+        salary_str = "Не указан"
+
+    return salary_str
